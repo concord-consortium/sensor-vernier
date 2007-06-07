45e6daed34963d8a0f885ae1bfd0498164b7d11c
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-06-07 21:27:17 +0000

    removed empty static block
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/sensor/sensor-vernier@105 6e01202a-0783-4428-890a-84243c50cc2b

--- src/main/java/org/concord/sensor/vernier/LabProSensorDevice.java
+++ src/main/java/org/concord/sensor/vernier/LabProSensorDevice.java
@@ -39,9 +39,6 @@ public class LabProSensorDevice extends AbstractStreamingSensorDevice
 	
 	protected String currentErrorMessage;
 	
-	static {
-	}
-	
     public LabProSensorDevice()
     {
     	deviceLabel = "LP";
