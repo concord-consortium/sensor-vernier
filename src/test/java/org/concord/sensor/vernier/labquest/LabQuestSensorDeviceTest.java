cbaeaeed41ffd8369ba81a4de378033c66b11d53
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2011-01-05 21:59:20 +0000

    Beginnings of SensorDevice Test Suite
    To use the suite extend the SensorDeviceTest and implement the setup method to create your sensor device type.  Currently PseudoSensorDeviceTest and LabQuestSensorDeviceTest are examples of this. 
    
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/sensor/sensor-vernier@23818 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/test/java/org/concord/sensor/vernier/labquest/LabQuestSensorDeviceTest.java
@@ -0,0 +1,11 @@
+package org.concord.sensor.vernier.labquest;
+
+import org.concord.sensor.device.SensorDeviceTest;
+
+
+public class LabQuestSensorDeviceTest extends SensorDeviceTest {
+	public void setup(){
+		device = new LabQuestSensorDevice();
+	}
+
+}
