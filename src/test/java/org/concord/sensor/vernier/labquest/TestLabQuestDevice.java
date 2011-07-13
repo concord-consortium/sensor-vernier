4638fe845456ab81ec50309bb6c0aff3a9d2bb31
sbannasch <sbannasch@6e01202a-0783-4428-890a-84243c50cc2b>
2011-07-13 18:31:44 +0000

    test Temperature AND Light on LabQuest
    The TestLabQuestDevice JUnit test now expects and tests
    for data from both a temperature and light probes.
    
    The temperature and light probes can be connected in
    any order to the physical ports.
    
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/sensor/sensor-vernier@24949 6e01202a-0783-4428-890a-84243c50cc2b

--- src/test/java/org/concord/sensor/vernier/labquest/TestLabQuestDevice.java
+++ src/test/java/org/concord/sensor/vernier/labquest/TestLabQuestDevice.java
@@ -4,10 +4,10 @@ import org.concord.sensor.device.impl.JavaDeviceFactory;
 import org.concord.sensor.test.TestInterfaceManager;
 import org.junit.Test;
 
-public class TestLabQuestDevice 
+public class TestLabQuestDevice
 {
 	@Test
-	public void testTemperature(){
-		TestInterfaceManager.testTemperature(JavaDeviceFactory.VERNIER_LAB_QUEST);
+	public void testTemperatureAndLight(){
+		TestInterfaceManager.testTemperatureAndLight(JavaDeviceFactory.VERNIER_LAB_QUEST);
 	}
 }
